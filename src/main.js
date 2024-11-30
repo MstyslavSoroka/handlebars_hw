51285692fb9a94696967a53b45e2663641c723b9
Mstyslav <134134015+MstyslavSoroka@users.noreply.github.com>
2024-11-30 09:22:32 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import template from '/template.handlebars';
-import items from '/countries.json';
+import template from './template.handlebars';
+import items from './countries.json';
 
 const list = document.querySelector(`.gallery`);
 const layout = template(items);
